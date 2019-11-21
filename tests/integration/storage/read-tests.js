abd568cfa19f2f44ce16ea64c08bde9319700cd0
Anton Gupalenko <anton.gupalenko@incountry.com>
2019-11-21 10:33:08 +0300

    FB_EN-1653 Adds test suite to read data

---
+++ tests/integration/storage/read-tests.js
@@ -0,0 +1,58 @@
+var Storage = require('../../../storage');
+var CryptKeyAccessor = require('../../../crypt-key-accessor');
+
+var expect = require('chai').expect;
+
+var storage, testBody, countryCode, keyValue;
+
+describe('Read data from Storage', function () {
+
+    before(async function () {
+        storage = new Storage(
+            {
+                tls: true,
+                encrypt: false
+            },
+            null,
+            new CryptKeyAccessor(function () { return 'supersecret'; })
+        );
+        countryCode = 'US'
+        keyValue = 'recordKey0'
+
+        testBody = JSON.stringify({ "name": "PersonName" });
+        await storage.writeAsync({
+            country: countryCode,
+            key: keyValue,
+            body: testBody
+        });
+    });
+
+
+    it('read existing data', async function () {
+
+        var readResponse = await storage.readAsync({
+            country: countryCode,
+            key: keyValue
+        });
+
+        expect(readResponse).to.exist;
+        expect(readResponse.status).to.equal(200);
+        expect(readResponse.data).to.exist;
+        expect(readResponse.data.body).to.equal(testBody);
+    }).timeout(20000);
+
+
+    it('read not existing data', async function () {
+
+        var notExistingKey = 'NotExistingKey11'
+        
+        var readResponse = await storage.readAsync({
+            country: countryCode,
+            key: notExistingKey
+        });
+
+        expect(readResponse).to.exist;
+        expect(readResponse.status).to.equal(404);
+        expect(readResponse.error).to.equal(`Could not find a record for key: ${notExistingKey}`)
+    }).timeout(30000);
+});
