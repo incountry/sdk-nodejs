bccbac7dd0ff77e2f1d01f083fd699392b54b2e6
Anton Gupalenko <anton.gupalenko@incountry.com>
2019-11-26 01:34:34 +0300

    FB_EN-1653 Adds dotenv

--- tests/integration/storage/common.js
+++ tests/integration/storage/common.js
@@ -1,5 +1,8 @@
+const dotenv = require('dotenv');
 const Storage = require('../../../storage');
 const SecretKeyAccessor = require('../../../secret-key-accessor');
+
+dotenv.config();
 
 /**
  * @param {Boolean} encryption - Encryption value
@@ -7,6 +10,9 @@ const SecretKeyAccessor = require('../../../secret-key-accessor');
 function CreateStorage(encryption) {
   return new Storage(
     {
+      apiKey: process.env.INC_API_KEY,
+      environmentId: process.env.INC_ENVIRONMENT_ID,
+      endpoint: process.env.INC_URL,
       encrypt: encryption,
     },
     new SecretKeyAccessor((() => 'supersecret')),
