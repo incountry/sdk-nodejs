d446579c6fa5c74aac72be8662a6082e9060532a
Anton Gupalenko <anton.gupalenko@incountry.com>
2019-11-27 13:42:46 +0300

    FB_EN-1653 Adds integration test for delete method

--- tests/integration/storage/delete-tests.js
+++ tests/integration/storage/delete-tests.js
@@ -4,47 +4,60 @@ const storageCommon = require('./common');
 const createStorage = storageCommon.CreateStorage;
 let storage;
 
-let testBody;
-let countryCode;
-let keyValue;
-
 describe('Delete data from Storage', () => {
   before(async () => {
     storage = createStorage(false);
-
-    countryCode = 'US';
-    keyValue = 'recordKey0';
-
-    testBody = JSON.stringify({ name: 'PersonName' });
-    await storage.writeAsync({
-      country: countryCode,
-      key: keyValue,
-      body: testBody,
-    });
   });
 
-
   it('C1885 Delete data', async () => {
+    const data = {
+      country: 'US',
+      key: 'recordKey201',
+      body: JSON.stringify({ name: 'PersonName' }),
+    };
+    const writeResponse = await storage.writeAsync(data);
+    expect(writeResponse.data).to.equal('OK');
+
     const deleteResponse = await storage.deleteAsync({
-      country: countryCode,
-      key: keyValue,
+      country: data.country,
+      key: data.key,
     });
 
-    // expect(deleteResponse).to.exist;
     expect(deleteResponse.status).to.equal(200);
   });
 
 
   it('C1886 Delete not existing data', async () => {
-    const notExistingKey = 'NotExistingKey11';
-
-    const deleteResponse = await storage.readAsync({
-      country: countryCode,
+    const notExistingKey = 'NotExistingKey123';
+    const deleteResponse = await storage.deleteAsync({
+      country: 'US',
       key: notExistingKey,
     });
 
-    // expect(deleteResponse).to.exist;
     expect(deleteResponse.status).to.equal(404);
     expect(deleteResponse.error).to.equal(`Could not find a record for key: ${notExistingKey}`);
+  });
+
+  describe('Encryption', () => {
+    before(async () => {
+      storage = createStorage(true);
+    });
+
+    it('C1920 Delete encrypted data', async () => {
+      const data = {
+        country: 'US',
+        key: 'recordEncKey0101',
+        body: JSON.stringify({ LastName: 'MyEncLastName' }),
+      };
+      const writeResponse = await storage.writeAsync(data);
+      expect(writeResponse.status).to.equal(201);
+
+      const deleteResponse = await storage.deleteAsync({
+        country: data.country,
+        key: data.key,
+      });
+
+      expect(deleteResponse.status).to.equal(200);
+    });
   });
 });
