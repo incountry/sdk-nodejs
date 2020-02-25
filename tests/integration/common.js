b149a6a8866e9449ce175ec9aca9b3d61a2c919c
Anton Gupalenko <anton.gupalenko@incountry.com>
2020-02-25 12:53:45 +0300

    EN-1883 Adds tests for normolize keys case

--- tests/integration/common.js
+++ tests/integration/common.js
@@ -6,14 +6,16 @@ dotenv.config();
 
 /**
  * @param {Boolean} encryption - Encryption value
+ * @param {Boolean} normalizeKeys - normalizeKeys value, default false
  */
-function createStorage(encryption) {
+function createStorage(encryption, normalizeKeys = false) {
   return new Storage(
     {
       apiKey: process.env.INC_API_KEY,
       environmentId: process.env.INC_ENVIRONMENT_ID,
       endpoint: process.env.INC_URL,
       encrypt: encryption,
+      normalizeKeys,
     },
     new SecretKeyAccessor((() => 'supersecret')),
   );
