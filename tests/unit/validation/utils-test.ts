d33dabd4b83fad5512451574062b1b2f3ea640cc
Igor Alekseev <igor.alekseev@incountry.com>
2020-09-01 17:27:03 +0300

    SDK-217 improve test coverage for default values in public methods

--- tests/unit/validation/utils-test.ts
+++ tests/unit/validation/utils-test.ts
@@ -6,7 +6,7 @@ import {
   toStorageClientError,
   toStorageServerError,
   validationToPromise,
-  withDefault,
+  optional,
   isValid,
 } from '../../../src/validation/utils';
 import {
@@ -16,6 +16,7 @@ import {
 
 chai.use(chaiAsPromised);
 const { expect, assert } = chai;
+// const { expect } = chai;
 
 describe('Validation Utils', () => {
   describe('toStorageClientError', () => {
@@ -39,7 +40,7 @@ describe('Validation Utils', () => {
     });
   });
 
-  describe('withDefault', () => {
+  describe('optional', () => {
     const codec = t.string;
 
     it('should decode valid value', () => {
@@ -52,9 +53,8 @@ describe('Validation Utils', () => {
       expect(isValid(codec.decode(undefined))).to.equal(false);
     });
 
-    context('same codec wrapped withDefault', () => {
-      const defaultValue = 'test';
-      const codecWithDefault = withDefault(codec, defaultValue);
+    context('same codec wrapped optional', () => {
+      const codecWithDefault = optional(codec);
 
       it('should decode valid value', () => {
         expect(isValid(codecWithDefault.decode(''))).to.equal(true);
@@ -62,12 +62,12 @@ describe('Validation Utils', () => {
       it('should return error for invalid value', () => {
         expect(isValid(codecWithDefault.decode(1))).to.equal(false);
       });
-      it('should decode predefined value for undefined input', () => {
+      it('should decode undefined as a valid value', () => {
         const result = codecWithDefault.decode(undefined);
         if (!isValid(result)) {
           throw assert.fail('codec wrapped withDefault should decode valid');
         }
-        expect(result.right).to.equal(defaultValue);
+        expect(result.right).to.equal(undefined);
       });
     });
   });
