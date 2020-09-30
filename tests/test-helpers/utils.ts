590a17e7cc9443bcb28e993c688bd814c0475e41
Igor Alekseev <igor.alekseev@incountry.com>
2020-09-30 14:33:30 +0300

    SDK-80 remove unused code

--- tests/test-helpers/utils.ts
+++ tests/test-helpers/utils.ts
@@ -1,20 +1,4 @@
 import { Stream } from 'stream';
-import { StorageRecord, StorageRecordAttachment } from '../../src/validation/storage-record';
-import { ApiRecord } from '../../src/validation/api/api-record';
-import { ApiRecordAttachment } from '../../src/validation/api/api-record-attachment';
-
-function fromStorageRecordAttachment(a: StorageRecordAttachment): ApiRecordAttachment {
-  return {
-    file_id: a.fileId,
-    filename: a.fileName,
-    hash: a.hash,
-    mime_type: a.mimeType,
-    size: a.size,
-    created_at: a.createdAt,
-    updated_at: a.updatedAt,
-    download_link: a.downloadLink,
-  };
-}
 
 function readStream(stream: Stream) {
   return new Promise((resolve, reject) => {
